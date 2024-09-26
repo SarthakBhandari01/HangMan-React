cf61fc5c407a94e6c9b41f562accaca90149b2d2
Sarthak <sarthakbhandari49@gmail.com>
2024-09-26 08:24:23 +0530

    added simple routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-import TextInputFormContainer from "./Component/TextInputForm/textInputFormContainer";
+import { Navigate, Route, Routes } from "react-router-dom";
 import StartGame from "./Component/Pages/start";
 import PlayGame from "./Component/Pages/Play";
 function App() {
@@ -8,17 +7,10 @@ function App() {
       <div className="w-full text-3xl px-3 h-[50px] bg-blue-200 flex items-center justify-center font-semibold text-black">
         Hang Man Game
       </div>
-      {/* <div className="text-3xl text-center font-semibold">
-          Welcome to HangMan
-        </div>
-        //{" "}
-        <TextInputFormContainer
-          onSubmit={(value) => console.log("submitted with the value", value)}
-        /> */}
       <Routes>
         <Route path="/start" element={<StartGame />} />
         <Route path="/play" element={<PlayGame />} />
-        <Route path="*" element={<div>Page not found</div>} />
+        <Route path="*" element={<Navigate to={"/start"} />} />
       </Routes>
     </>
   );
